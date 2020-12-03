2f33f0a482b921263f16540f9d46c01a8041c527
Mishelinus <mishelin06@yandex.ru>
2020-12-03 13:17:17 +0300

    Добавил page.html, сделал адаптацию header и footer под мобильные устройства, устранил косяки

--- app/js/main.js
+++ app/js/main.js
@@ -13,6 +13,24 @@ $(function(){
          arrows:false
     })
 
+
+    // gamburger menu
+    $('.menu__btn').on('click', function(){
+$('.menu__list').slideToggle();
+    });
+
+    //прячем header__box  
+    $('.header__btn-menu').on('click', function(){
+$('.header__box').toggleClass('active');
+});
+
+
+  
+    
+// Обязательно Должен быть самом конце кода. 
+//иначе всё, что после него не будет работать, 
+//если не найдёт себя в доме 
 var mixer = mixitup('.products__inner-box');
 
 });
+
