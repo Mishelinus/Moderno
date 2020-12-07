8e36369b5648ce6d95e4c14cd2da06067a121a10
Mishelinus <mishelin06@yandex.ru>
2020-12-07 19:21:29 +0300

    ссоздал страницу product-one.html с использованием табов. tabs forked from Scrydz/Tabs.js

--- app/js/main.js
+++ app/js/main.js
@@ -45,6 +45,17 @@ $(".js-range-slider").ionRangeSlider({
     $('.icon-th-list').removeClass('active');
   });
 
+  // start tabs
+  $('.product-one__tabs .tab').on('click', function(event) {
+    var id = $(this).attr('data-id');
+      $('.product-one__tabs').find('.tab-item').removeClass('active-tab').hide();
+      $('.product-one__tabs .tabs').find('.tab').removeClass('active');
+      $(this).addClass('active');
+      $('#'+id).addClass('active-tab').fadeIn();
+      return false;
+    });
+  // end tabs
+
 
   
     
