ba087061adc4140a7691ee2303899aaa47b853bf
Mishelinus <mishelin06@yandex.ru>
2021-01-26 01:34:34 +0300

    added page withdrawals.html

--- app/js/main.js
+++ app/js/main.js
@@ -67,7 +67,7 @@ $(".js-range-slider").ionRangeSlider({
 
   // стилизация select плагином Chosen v1.8.7
   $(".my_select_box").chosen({
-    width: "95%",
+    width: "100%",
   disable_search: false,
   disable_search_threshold: 5,
   enable_split_word_search: false,
